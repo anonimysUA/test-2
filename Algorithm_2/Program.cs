639656f42a280e750ac4fe951a2a9076ef43822f
Dmytro Udielov <Dmytro Udielov>
2016-04-02 18:41:45 +0300

    SofthemeSchool A_2

---
+++ Algorithm_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Algorithm_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (;;)
+            {
+                try
+                {
+                    Console.Write("Input number: ");
+                    int input = Int32.Parse(Console.ReadLine());
+                    for (int i = 1; i <= input; i++)
+                    {
+                        if ((input % i) == 0)
+                        {
+                            Console.WriteLine(i);
+                        }
+                    }
+                    Console.WriteLine();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Input numbers please");
+                }
+
+
+            }
+        }
+    }
+}
